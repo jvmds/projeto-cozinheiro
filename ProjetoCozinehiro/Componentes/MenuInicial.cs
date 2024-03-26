6dca60ca6529b8f8929b557e61a8863ea9f5e65a
jvmds <joaovitormelodossantos.1@gmail.com>
2024-03-26 18:51:09 -0300

    base projeto

---
+++ ProjetoCozinehiro/Componentes/MenuInicial.cs
@@ -0,0 +1,54 @@
+namespace CozinheiroRpg.Componentes;
+
+public class MenuInicial
+{
+    public bool Execultar()
+    {
+      var texto = """
+                 _____  ______ _____                           
+                |  __ \|  ____|_   _|                          
+                | |__) | |__    | |                            
+                |  _  /|  __|   | |                            
+                | | \ \| |____ _| |_                           
+                |_|__\_\______|_____|                          
+                |  __ \   /\                                   
+                | |  | | /  \                                  
+                | |  | |/ /\ \                                 
+                | |__| / ____ \                                
+                |_____/_/___ \_\________ _   _ _    _          
+                 / ____/ __ \___  /_   _| \ | | |  | |   /\    
+                | |   | |  | | / /  | | |  \| | |__| |  /  \   
+                | |   | |  | |/ /   | | | . ` |  __  | / /\ \  
+                | |___| |__| / /__ _| |_| |\  | |  | |/ ____ \ 
+                 \_____\____/_____|_____|_| \_|_|  |_/_/    \_\
+      """;
+
+      foreach (var linha in texto.Split('\n'))
+      {
+          Console.WriteLine(linha);
+          Thread.Sleep(100);
+      }
+      
+      Console.WriteLine('\n');
+      var visivel = false;
+      while (!Console.KeyAvailable)
+      {
+          visivel = !visivel;
+          Console.SetCursorPosition(0, Console.CursorTop);
+          if (visivel)
+          {
+              Console.Write("          <<< Digite qualquer tecla para iniciar! >>>");
+          }
+          else
+          {
+              Console.Write(new string(' ', 53));
+          }
+          
+          Thread.Sleep(700);
+      }
+
+      Console.ReadKey(true);
+      Console.Clear();
+      return true;
+    }
+}
