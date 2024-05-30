9000ff517b08a9b2c7d6f7299b3d910c860bd961
jvmds <joaovitormelodossantos.1@gmail.com>
2024-05-30 13:16:10 -0300

    remoção de arquivos desnecessários

--- Common/DTOs/ConfiguracaoMenu.cs
+++
@@ -1,6 +0,0 @@
-namespace Common.DTOs;
-
-public record ConfiguracaoMenu(
-                char Margem = '*', 
-                int TamanhoMargemExterna = 5, 
-                int TamanhoMargemInterna = 1);
